5dd92c0dbea9c16b11bb4f1574217325622fbc0d
Giratin <yassine.sta@zumrails.com>
2022-09-26 11:13:45 +0100

    correction exerice atelier 1

---
+++ index.js
@@ -0,0 +1,64 @@
+import Express from "express";
+import { readFile, readFileSync } from "fs";
+
+
+const port = process.env.PORT || 9090;
+const hostname = '127.0.0.1';
+
+const app = Express();
+
+class Game{
+    constructor(name, year, URL){
+        this.name= name;
+        this.year = year;
+        this.link =  URL;
+    }
+}
+
+
+const getGames = (clb) => {
+    readFile("./SteamGames.json", (err,data)=>{
+        if(err){
+            clb(err, null)
+        }
+        const list = JSON.parse(data);
+        const games = list.map((el) => new Game(el.Game, el.Year, el.GameLink));
+       clb(null, games)
+    })
+}
+
+app.get('/game', (req,res)=>{
+
+    getGames((err,data)=>{
+        if(err){
+            return res.status(404).send(err)
+        }
+        res.json(data)
+    })
+
+    // readFile("./SteamGames.json", (err,data)=>{
+    //     if(err){
+    //         return res.status(404).send(err)
+    //     }
+        
+    //     const list = JSON.parse(data);
+    //     const games = list.map((el) => new Game(el.Game, el.Year, el.GameLink));
+    //     res.json(games)
+    // })
+});
+
+app.get("/game/select/:year" , (req,res)=>{
+    const year = +(req.params.year)
+    getGames((err,data)=>{
+        if(err){
+            return res.status(404).send(err)
+        }
+        const games = data.filter((el) => el.year > year);
+        res.status(200).json(games)
+    })
+})
+
+
+app.listen(port, hostname, ()=>{
+    console.log(`Server is running on http://${hostname}:${port}`);
+})
